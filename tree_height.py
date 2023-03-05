851885ae383470a0ac04c0d03ef96e7bb1efe0cd
221rdb087 <125808567+221rdb087@users.noreply.github.com>
2023-03-05 13:47:19 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,33 +1,37 @@
-# python3
-
 import sys
 import threading
-import numpy
 
-
+# modified compute_height function
 def compute_height(n, parents):
-    # Write this function
-    children = {}    
-    for i in range(n):
-        children[i] = []
+    # create a dictionary to store children of each node
+    children = {i: [] for i in range(n)}
+
+    # fill in the children dictionary
     for i, parent in enumerate(parents):
-        if parent == -1:
-            root = i
-        else:
+        if parent != -1:
             children[parent].append(i)
+
+    # recursive function to calculate the height of each node
     def calc_height(node):
         if not children[node]:
-            return 0
+            return 1
         else:
-            return 1+max([calc_height(child) for child in children[node]])
-    return calc_height(root)
-        
+            return 1 + max([calc_height(child) for child in children[node]])
+
+    # calculate the height of each node and return the maximum height
+    max_height = 0
+    for i in range(n):
+        height = calc_height(i)
+        if height > max_height:
+            max_height = height
+    return max_height
 
 def main():
+    # read input
     source = input()
     if source == "I":
         n = int(input())
-        parents = list(map(int, input().strip().split()))
+        parents = list(map(int, input().split()))
     elif source == "F":
         file_name = input().strip()
         if "a" in file_name:
@@ -40,12 +44,11 @@ def main():
             sys.exit()
     else:
         sys.exit()
-        
+
     max_height = compute_height(n, parents)
     print(max_height)
 
-sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
+sys.setrecursionlimit(10**7)
+threading.stack_size(2**27)
+
 threading.Thread(target=main).start()
-main()
-# print(numpy.array([1,2,3]))
